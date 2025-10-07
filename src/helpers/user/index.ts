13baa6cd11dfa06ba08e0c848300e53e0c238943
Michael Seveen <michaelseveen8@gmail.com>
2025-10-07 16:06:27 +0100

    Removed redirect from user helper

--- src/helpers/user/index.ts
+++ src/helpers/user/index.ts
@@ -2,16 +2,11 @@ import "server-only";
 
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import { redirect } from "next/navigation";
 
 export const getCurrentUser = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if (!session) {
-    return redirect("/auth/sign-in");
-  }
-
-  return session.user;
+  return session?.user ?? null;
 };
